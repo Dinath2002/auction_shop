a1d34d5369cd74f910393eac3fe2e760d9f28672
Dinath2002 <sdinath0528@gmail.com>
2025-11-10 21:29:37 +0530

    Add home view using TemplateView for landing page

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+
 class HomeView(TemplateView):
-    template_name = 'core/base.html'
+    template_name = 'core/home.html'
+
 
 # Create your views here.
