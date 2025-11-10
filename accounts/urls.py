8abd69e954f328d9a81e60fa9ba21f70e9a69cb0
Dinath2002 <sdinath0528@gmail.com>
2025-11-10 21:27:57 +0530

    Add signup URL pattern for user registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import SignUpView
+from . import views
 
-app_name = 'accounts'
-urlpatterns = [ path('signup/', SignUpView.as_view(), name='signup') ]
+urlpatterns = [
+    path('signup/', views.signup_view, name='signup'),
+]
