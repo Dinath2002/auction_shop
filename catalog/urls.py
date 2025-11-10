53c9243d8b4754081a50164572cedb0a1d7fb8cf
Dinath2002 <sdinath0528@gmail.com>
2025-11-10 21:28:12 +0530

    Add product list and detail URLs for catalog app

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
+from .views import ProductListView, ProductDetailView
 
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', views.ProductListView.as_view(), name='product_list'),
-    path('<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('', ProductListView.as_view(), name='product_list'),
+    path('<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
 ]
