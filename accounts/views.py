fc7ca421c6a02d6c540cab87996e566b7cb1f33a
Dinath2002 <sdinath0528@gmail.com>
2025-11-10 21:28:58 +0530

    Implement user signup view using Django UserCreationForm

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+# accounts/views.py
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
-
-# Create your views here.
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # redirect to your homepage
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
